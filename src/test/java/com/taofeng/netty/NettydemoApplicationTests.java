6d297d39b645f053ad8e048555df2196d5363655
乐陶 <letao@maihaoche.com>
2019-11-28 17:43:51 +0800

    首次提交

---
+++ src/test/java/com/taofeng/netty/NettydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taofeng.netty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
